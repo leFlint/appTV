7437cbd00d0f7bb50d410de37178962acf686fcd
Ala-Eddine OUAZANI <Ala-Eddine OUAZANI>
2016-11-27 18:00:55 +0100

    feat(init): ajout du fichier resource + initi du deserializer

--- src/main/java/fr/epsi/appTV/dao/JSONDeserializer.java
+++ src/main/java/fr/epsi/appTV/dao/JSONDeserializer.java
@@ -1,8 +1,16 @@
 package fr.epsi.appTV.dao;
 
-
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import fr.epsi.appTV.models.Serie;
+
+import java.util.List;
+
 
 public class JSONDeserializer {
-    Gson
+    public static final String JSON_PATH = "/resources/db/series.json";
+
+    Gson gson = new GsonBuilder().create();
+    List<Serie> serie = gson.fromJson(JSON_PATH, List.class);
+
 }
